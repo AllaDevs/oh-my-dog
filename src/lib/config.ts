2aa329b7761536f945d7aa48f0597b8e88b5e370
Juan-Suarezz <128266846+Juan-Suarezz@users.noreply.github.com>
2023-06-29 17:29:15 -0300

    Better integration of ngrok

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
-export const NGROK_URL = `https://507f-190-19-7-112.sa.ngrok.io`;
+export const NGROK_URL = `https://8e54b21f7d3d-59178990996674649.ngrok-free.app`;
 
 export const DOG_IMAGE_MAX_SIZE = 4 * 1024 * 1024; // 4mb
 
